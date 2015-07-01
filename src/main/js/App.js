5cd39eeb995b4fb0fb7de8e7be63e549b8f36f5a
Adam York <vertigo.index@gmail.com>
2015-07-01 13:40:10 -0400

    removing jsdom for cheerio

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,33 +1,35 @@
-"use strict";
-$( document ).ready( function() {
+'use strict';
+$(document).ready(function() {
 
-    var _names = [ "Hacker News", "Lobsters", "Arstechnica", "Slashdot" ];
-    var _paths = [ "/hn", "/lob", "/ars", "sd" ];
+    var JST = window.JST;
+
+    var _names = ['Hacker News', 'Lobsters', 'Arstechnica', 'Slashdot'];
+    var _paths = ['/hn', '/lob', '/ars', '/sd'];
     var _columns;
 
-    var _addColumn = function( name, path, index ) {
-        $.get( path, function( data ) {
-            var column = _columns.eq( index );
-            column.append( JST.header( {
+    var _addColumn = function(name, path, index) {
+        $.get(path, function(data) {
+            var column = _columns.eq(index);
+            column.append(JST.header({
                 source: name
-            } ) );
-            _.each( data.articles, function( article ) {
-                column.append( JST.article( {
+            }));
+            _.each(data.articles, function(article) {
+                column.append(JST.article({
                     href: article.href,
                     desc: article.desc
-                } ) );
-            } );
-        } );
+                }));
+            });
+        });
     };
 
     var _init = function() {
-        $( "body" ).append( JST.columns() );
-        _columns = $( "body" ).children( "#columns" ).children( ".column" );
-        _.each( _names, function( name, i ) {
-            _addColumn( name, _paths[ i ], i );
-        } );
+        $('body').append(JST.columns());
+        _columns = $('body').children('#columns').children('.column');
+        _.each(_names, function(name, i) {
+            _addColumn(name, _paths[i], i);
+        });
     };
 
     _init();
 
-} );
+});
--- src/main/node/server.js
+++ src/main/node/server.js
@@ -1,163 +1,135 @@
-"use strict";
-var express = require( "express" );
-var http = require( "http" );
-var https = require( "https" );
-var jsdom = require( "jsdom" );
+'use strict';
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var cheerio = require('cheerio');
 var app = express();
-var und = require( 'underscore-node' );
+var und = require('underscore-node');
 
-app.set( "view engine", "ejs" );
-app.set( "views", __dirname );
-app.get( "/", function( req, res ) {
-    res.render( "index.ejs", {
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+app.get('/', function(req, res) {
+    res.render('index.ejs', {
         layout: false
-    } );
-} );
+    });
+});
 
-var _toTitleCase = function( str ) {
-    var words = str.split( " " );
-    words = und.map( words, function( word ) {
-        var firstChar = word.substr( 0, 1 );
-        var remaining = word.substr( 1, word.length );
+var _toTitleCase = function(str) {
+    var words = str.split(' ');
+    words = und.map(words, function(word) {
+        var firstChar = word.substr(0, 1);
+        var remaining = word.substr(1, word.length);
         return firstChar.toUpperCase() + remaining;
-    } );
-    return words.join( " " );
+    });
+    return words.join(' ');
 };
 
-var _getResource = function( options, prot, callback ) {
-    var req = prot.get( options, function( res ) {
-        var chunks = [];
-        res.on( "data", function( chunk ) {
-            chunks.push( chunk );
-        } );
-        res.on( "end", function() {
-            var body = Buffer.concat( chunks );
-            callback( body );
-        } );
-        req.on( "error", function( e ) {
-            callback( e.message, true );
-        } );
-    } );
+var _getResource = function(options, prot, callback) {
+    var req = prot.get(options, function(res) {
+        var chunks = '';
+        res.on('data', function(chunk) {
+            chunks += chunk;
+        });
+        res.on('end', function() {
+            callback(chunks);
+        });
+        req.on('error', function(e) {
+            callback(e.message, true);
+        });
+    });
 };
 
-var _processDom = function( data, res, postProcess ) {
-    jsdom.env( {
-        html: data,
-        scripts: [
-            '../bower_components/jquery/dist/jquery.js',
-        ],
-        done: function( errors, window ) {
-            if ( errors ) {
-                var errorStr = "An error occured when processing the document.\n";
-                und.each( errors, function( error ) {
-                    errorStr += ( error.message + "\n" );
-                } );
-                res.send( {
-                    articles: [ {
-                        href: "",
-                        desc: errorStr
-                    } ]
-                } );
-                return;
-            }
-            var $ = window.$;
-            var items = postProcess( $ );
-            res.send( {
-                articles: items
-            } );
-        }
-    } );
+var _processDom = function(data, res, postProcess) {
+    var $ = cheerio.load(data);
+    var items = postProcess($);
+    res.send({
+        articles: items
+    });
 };
 
-var _mapRequest = function( resource, host, path, prot, postProcess, useDom ) {
-    app.get( resource, function( req, res ) {
-        _getResource( {
+var _mapRequest = function(resource, host, path, prot, postProcess, useDom) {
+    app.get(resource, function(req, res) {
+        _getResource({
             host: host,
             path: path
-        }, prot, function( data, error ) {
-            if ( error ) {
-                res.send( {
-                    articles: [ {
-                        href: "",
+        }, prot, function(data, error) {
+            if (error) {
+                res.send({
+                    articles: [{
+                        href: '',
                         desc: data
-                    } ]
-                } );
+                    }]
+                });
                 return;
             }
-            if ( useDom ) {
-                _processDom( data, res, postProcess );
+            if (useDom) {
+                _processDom(data, res, postProcess);
             } else {
-                res.send( {
-                    articles: postProcess( data )
-                } );
+                res.send({
+                    articles: postProcess(data)
+                });
             }
-        } );
-    } );
+        });
+    });
 };
 
-var _postProcessHackerNews = function( data ) {
-    try {
-        var items = JSON.parse( data ).items;
-        items = und.map( items, function( item ) {
-            return {
-                href: item.url,
-                desc: _toTitleCase( item.title )
-            };
-        } );
-        return items;
-    } catch ( err ) {
-        var items = [ {
-            href: "",
-            desc: "An error occured when processing the document."
-        } ];
-        return items;
-    }
-};
-
-var _postProcessLobsters = function( $ ) {
-    var items = $( ".link a" );
-    items = und.map( items, function( item ) {
+var _postProcessHackerNews = function($) {
+    var items = $('.athing td:nth-child(3) a');
+    items = und.map(items, function(item) {
+        var target = item.attribs.href;
+        var base = 'https://news.ycombinator.com/';
         return {
-            href: item.href,
-            desc: _toTitleCase( item.innerHTML )
+            href: (target.indexOf('item?id=') === -1) ? target : base + target,
+            desc: _toTitleCase(item.children[0].data)
         };
-    } );
+    });
     return items;
 };
 
-var _postProcessArstechnica = function( $ ) {
-    var items = $( ".post article > a" ).toArray();
-    items = und.map( items, function( item ) {
-        var descText = item.href.toString();
-        var parts = descText.split( "/" );
-        var descParsed = parts[ parts.length - 2 ];
-        var noHyphens = descParsed.split( "-" ).join( " " );
+var _postProcessLobsters = function($) {
+    var items = $('.link a');
+    items = und.map(items, function(item) {
         return {
-            href: item.href,
-            desc: _toTitleCase( noHyphens )
+            href: item.attribs.href,
+            desc: _toTitleCase(item.children[0].data)
         };
-    } );
+    });
     return items;
 };
 
-var _postProcessSlashdot = function( $ ) {
-    var items = $( ".story-header h1 a" );
-    items = und.map( items, function( item ) {
-        var replaced = item.href.replace( "file", "http" );
+var _postProcessArstechnica = function($) {
+    var items = $('.post article > a').toArray();
+    items = und.map(items, function(item) {
+        var descText = item.attribs.href.toString();
+        var parts = descText.split('/');
+        var descParsed = parts[parts.length - 2];
+        var noHyphens = descParsed.split('-').join(' ');
         return {
-            href: replaced,
-            desc: _toTitleCase( item.innerHTML )
+            href: item.attribs.href,
+            desc: _toTitleCase(noHyphens)
         };
-    } );
+    });
     return items;
 };
-_mapRequest( "/hn", "api.ihackernews.com", "/page", http, _postProcessHackerNews );
-_mapRequest( "/lob", "lobste.rs", "/", https, _postProcessLobsters, true );
-_mapRequest( "/ars", "arstechnica.com", "/", http, _postProcessArstechnica, true );
-_mapRequest( "/sd", "beta.slashdot.org", "/", http, _postProcessSlashdot, true );
 
-app.use( "/", express.static( __dirname ) );
-app.listen( 8080 );
+var _postProcessSlashdot = function($) {
+    var items = $('.story span:first-child a').toArray();
+    items = und.map(items, function(item) {
+        return {
+            href: item.attribs.href,
+            desc: _toTitleCase(item.children[0].data)
+        };
+    });
+    return items;
+};
 
-console.log( "local application server started on port 8080." );
-console.log( "ctrl-c to exit." );
+_mapRequest('/hn', 'news.ycombinator.com', '/', https, _postProcessHackerNews, true);
+_mapRequest('/lob', 'lobste.rs', '/', https, _postProcessLobsters, true);
+_mapRequest('/ars', 'arstechnica.com', '/', http, _postProcessArstechnica, true);
+_mapRequest('/sd', 'slashdot.org', '/', http, _postProcessSlashdot, true);
+
+app.use('/', express.static(__dirname));
+app.listen(8080);
+
+console.log('local application server started on port 8080.');
+console.log('ctrl-c to exit.');
